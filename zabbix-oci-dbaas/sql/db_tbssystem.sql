91d5f6e7f0c7dfc9f08cd8ee810e094e7f06da34
yu araki <a2105717@gmail.com>
2019-09-13 15:16:44 +0900

    Add files via upload

---
+++ zabbix-oci-dbaas/sql/db_tbssystem.sql
@@ -0,0 +1,8 @@
+SET pagesize 0
+SET heading OFF
+SET feedback OFF
+SET verify OFF
+select round((min(a.BYTES)/(1024*1024) - sum(b.BYTES)/(1024*1024))/ (min(a.BYTES)/1024/1024)*100,2)
+from dba_data_files a, dba_free_space b
+where a.FILE_ID = b.FILE_ID and a.TABLESPACE_NAME = 'SYSTEM';
+QUIT;
